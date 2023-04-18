956394ab63a8c6bf65d273bbbdbc43e6d901cbab
Erico Darmawan Handoyo <khe.wan.xing@outlook.com>
2023-04-18 10:46:51 +0700

    Added Source Code & Asset for ObjectBox Study Case

---
+++ ObjectBox Study Case - ol10/lib/presentation/assets.dart
@@ -0,0 +1,21 @@
+abstract class Assets {
+  static const String forum = "assets/forum.png";
+
+  static const String goals = "assets/goals.png";
+
+  static const String home = "assets/home.png";
+
+  static const String message = "assets/message.png";
+
+  static const String selectedForum = "assets/selected_forum.png";
+
+  static const String selectedGoals = "assets/selected_goals.png";
+
+  static const String selectedHome = "assets/selected_home.png";
+
+  static const String selectedMessage = "assets/selected_message.png";
+
+  static const String logo = "assets/logo.png";
+
+  static const String bgpic = "assets/bgpic.jpg";
+}
---
+++ ObjectBox Study Case - ol10/lib/presentation/color_pallete.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color ol10Green = Color(0xff00A59B);
+const Color ol10PaleGreen = Color(0xffDCEAE9);
+const Color ol10Magenta = Color.fromARGB(255, 227, 69, 169);
+const Color ol10Yellow = Color(0xffECC02F);
+const Color ol10Blue = Color(0xff3C50C1);
